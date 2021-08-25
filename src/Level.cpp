448de080ea247c2284594079035b6541e893b7a8
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-25 15:00:41 -0300

    Adição de vidas

--- src/Level.cpp
+++ src/Level.cpp
@@ -9,9 +9,10 @@ using namespace std;
 Level::Level(int lin, int col, int com, vector<string> map){
     linhas = lin;
     colunas = col;
-    comidaRes = com;
-    comidaTot = com;
+    comidaTot = comidaRes = com;
     mapa = map;
+    score = 0;
+    vidaRes = vidaTot = 5;
 
     for(int i=0; i<mapa.size(); i++)
         for(int j=0; j<mapa[i].size(); j++){
@@ -32,25 +33,48 @@ Level::Level(int lin, int col, int com, vector<string> map){
     for(int i=0; i<comidaRes; i++){
        posComidas.push_back( espacos[rand() % espacos.size() + 1] );
     }
+    comida.linha = posComidas[comidaRes-1].linha;
+    comida.coluna = posComidas[comidaRes-1].coluna;
 }
 int Level::getComidaTotal(){
     return comidaTot;
 }
 int Level::getJaComidas(){
-    return comidaTot - comidaRes -1;
+    return comidaTot - comidaRes;
 }
 Pos Level::getPosComida(){
     return comida;
 }
-void Level::nextFood(){
-    comidaRes--;
-    comida.linha = posComidas[comidaRes].linha;
-    comida.coluna = posComidas[comidaRes].coluna;
-    if(comidaRes<0)
-        comida.linha=-1;
+int Level::getVidaTot(){
+    return vidaTot;
+}
+int Level::getVidaRes(){
+    return vidaRes;
+}
+int Level::getScore(){
+    return score;
 }
 Pos Level::getInicio(){
     return inicio;
+}
+void Level::perdeuLife(){
+    vidaRes--;
+    score = 0;
+    comidaRes = comidaTot;
+    posComidas.clear();
+    srand (time(NULL));
+    for(int i=0; i<comidaTot; i++){
+       posComidas.push_back( espacos[rand() % espacos.size() + 1] );
+    }
+    comida.linha = posComidas[comidaRes-1].linha;
+    comida.coluna = posComidas[comidaRes-1].coluna;
+}
+void Level::nextFood(){
+    comidaRes--;
+    comida.linha = posComidas[comidaRes-1].linha;
+    comida.coluna = posComidas[comidaRes-1].coluna;
+    if(comidaRes<1)
+        comida.linha=-1;
 }
 vector<string> Level::getMapa(){
     return mapa;
