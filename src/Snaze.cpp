7006940336d456a1e17957029b65f9f70bcb8241
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-25 00:09:49 -0300

    Passagem por argumento

--- src/Snaze.cpp
+++ src/Snaze.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    SnakeGame game;
-    
+    bool temRabo;
+    if(argv[1]=="1") temRabo=true;
+    else temRabo = false;
+
+    SnakeGame game(temRabo, argv[2]);
     game.loop(); //bloqueia aqui e só saí quando o jogo termina
+    
 
     return 0;
 }
